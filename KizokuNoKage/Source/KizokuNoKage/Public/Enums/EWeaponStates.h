2cf002e67e2098a4edeb46afc8df562fe944eed6
Sherwin Espela <sherwinjai_espela@yahoo.ca>
2025-05-07 09:48:12 -0700

    Sword Interaction system wip 5

--- KizokuNoKage/Source/KizokuNoKage/Public/Enums/EWeaponStates.h
+++ KizokuNoKage/Source/KizokuNoKage/Public/Enums/EWeaponStates.h
@@ -5,5 +5,6 @@ UENUM(BlueprintType)
 enum class EWeaponStates : uint8
 {
 	EWS_Equipped	UMETA(DisplayName = "Weapon Equipped"),
-	EWS_Unequipped	UMETA(DisplayName = "Weapon Unequipped")
+	EWS_Unequipped	UMETA(DisplayName = "Weapon Unequipped"),
+	EWS_Updating	UMETA(DisplayName = "Weapon State Updating")
 };
